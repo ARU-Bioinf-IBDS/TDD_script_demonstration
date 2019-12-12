5178e7e54b68bdd13d9916375c5184e57f51b909
Oliver Smart <oliver.s.smart@gmail.com>
2019-12-12 17:31:13 +0000

    Convert to a script (3) add main and (4) add debug print
    See GitHub issue page #1 for description of thought process.
    
    Manually test script.
    
    Test A: supply a sequence - looks good the sequence is printed
    in debug output
    
       $ ./complement.py ACGT
       debug sequence=ACGT reverse=False
    
    Test B: supply a sequence and -reverse looks good.
    
       $ ./complement.py -reverse TATATAA
       debug sequence=TATATAA reverse=True
    
    Test C: do not supply a sequence get a sensible error message
    
       $ ./complement.py
       usage: complement.py [-h] [-reverse] SEQUENCE
       complement.py: error: the following arguments are required: SEQUENCE
    
    Test D: look at the help message - not good does not explain what the
    script does. Does not give help on what the reverse flag does.
    
       $ ./complement.py -h
       usage: complement.py [-h] [-reverse] SEQUENCE
    
       positional arguments:
         SEQUENCE
    
       optional arguments:
         -h, --help  show this help message and exit
         -reverse

--- complement.py
+++ complement.py
@@ -29,3 +29,17 @@ def complement(seq):
     """ Returns the complement of the input DNA sequence """
     complement_mapping = str.maketrans('ATGC', 'TACG')
     return seq.translate(complement_mapping)
+
+def main(test_override=None):
+    """main function invoked when run as a script
+       override argument allows a list of arguments to be used in
+       place of the command-line arguments for testing purposes
+    """
+    sequence, reverse = parse_command_line_args(
+                        test_override=test_override)
+    print(f'debug sequence={sequence} reverse={reverse}')
+
+
+if __name__ == '__main__':
+    # only run if script is invoked not on import
+    main()
