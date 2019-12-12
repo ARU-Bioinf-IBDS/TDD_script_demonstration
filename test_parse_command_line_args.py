be3c80422956096c5c563610a7fd59eccdffbc7c
Oliver Smart <oliver.s.smart@gmail.com>
2019-12-12 17:50:42 +0000

    add a test that help message contains description and help for -reverse.
    See GitHub issue page #1 for description of thought process.
    and link to capturing stdout
    
    Test fails as have not yet added help.
    
    Failure shows stdout captured OK:
    
    	 pytest -q test_parse_command_line_args.py
    	..F...                                                                                 [100%]
    	========================================== FAILURES ==========================================
    	_____________________________________ test_help_message ______________________________________
    
    	capsys = <_pytest.capture.CaptureFixture object at 0x10790c2b0>
    
    	    def test_help_message(capsys):
    		"""test -h option that raises SystemExit and
    		writes a help message to standard output.
    		Capture the stdout using capsys
    		"""
    		with pytest.raises(SystemExit):
    		    parse_command_line_args(test_override=['-h'])
    		help_message = capsys.readouterr().out
    	>       assert 'prints complement of the input DNA sequence' in help_message
    	E       AssertionError: assert 'prints complement of the input DNA sequence' in 'usage: pytest [-h] [-reverse] SEQUENCE\n\npositional arguments:\n  SEQUENCE\n\noptional arguments:\n  -h, --help  show this help message and exit\n  -reverse\n'
    
    	test_parse_command_line_args.py:26: AssertionError

--- test_parse_command_line_args.py
+++ test_parse_command_line_args.py
@@ -15,9 +15,19 @@ def test_supply_sequence_and_minus_filter():
     assert parse_command_line_args(test_override=args) == ('TEST', True)
 
 
-def test_help_message():
-    # TODO osmart 12 Dec 2019
-    pass
+def test_help_message(capsys):
+    """test -h option that raises SystemExit and
+    writes a help message to standard output.
+    Capture the stdout using capsys
+    """
+    with pytest.raises(SystemExit):
+        parse_command_line_args(test_override=['-h'])
+    help_message = capsys.readouterr().out
+    assert 'prints complement of the input DNA sequence' in help_message
+    assert 'usage' in help_message
+    assert 'help' in help_message
+    assert 'prints the reverse complement rather ' \
+           'than the forward complement' in help_message
 
 
 def test_with_empty_args_raises_error():
