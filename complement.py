127bd27b9680f79eb56c0b02608b3919d05a4de7
Oliver Smart <oliver.s.smart@gmail.com>
2019-12-12 16:27:55 +0000

    non-functional stub parse_command_line_args with docstring.
    See GitHub issue page #1 for description of thought process.

--- complement.py
+++ complement.py
@@ -1,6 +1,20 @@
 """ script to calculate complement of a DNA sequence """
 
 
+def parse_command_line_args(test_override=None):
+    """Parse options from the command line, unless override
+    list test_override is specified (this is for testing).
+
+    Sets up the command line options using argparse including
+    the help message.
+
+    returns a tuple containing:
+      (sequence (str),
+       reverse (boolean)
+      )
+    """
+
+
 def complement(seq):
     """ Returns the complement of the input DNA sequence """
     complement_mapping = str.maketrans('ATGC', 'TACG')
